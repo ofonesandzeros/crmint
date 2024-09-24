40f500ae9dadc4052c4f2393c0d7e711ba4cbd28
instant-bqml <78978926+instant-bqml@users.noreply.github.com>
2024-09-23 23:32:48 -0400

    Handle duplicate tasks (#91)
    * Handle duplicate tasks
    
    * Set all jobs in pipeline to idle

--- backend/controller/models.py
+++ backend/controller/models.py
@@ -869,7 +869,11 @@ class Job(extensions.db.Model):
           pipeline_id=self.pipeline_id,
           job_id=self.id
         )
-        return 0  # No further action needed
+        for job in self.pipeline.jobs:
+          job.set_status(Job.STATUS.IDLE)
+        self.pipeline.set_status(Pipeline.STATUS.IDLE)
+        self.pipeline.leaf_job_finished()
+        return 0
       
       # Log warning and set job status to FAILED to force pipeline conclusion
       crmint_logging.log_message(
