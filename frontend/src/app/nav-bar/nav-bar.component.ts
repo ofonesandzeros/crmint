553ec1894fe39c5394f27acb79f716084811cbb5
instant-bqml <78978926+instant-bqml@users.noreply.github.com>
2024-10-18 21:57:57 -0400

    updates

--- frontend/src/app/nav-bar/nav-bar.component.ts
+++ frontend/src/app/nav-bar/nav-bar.component.ts
@@ -14,6 +14,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'environments/environment';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -23,9 +24,13 @@ import { environment } from 'environments/environment';
 export class NavBarComponent implements OnInit {
   enabled_stages: boolean = environment.enabled_stages;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  goToPipelines() {
+    this.router.navigate(['/pipelines'], { queryParams: { page: 1 } });
   }
 
 }
